81c52ce7fbddc390556d1cb6f1c5b18eef07db0f
DESKTOP-FA5A1AB\Chris <chris.elias101@gmail.com>
2023-12-04 20:07:12 +0000

    Added before steps

--- tests/contactFormTests.spec.js
+++ tests/contactFormTests.spec.js
@@ -1,9 +1,14 @@
 import { test } from '@playwright/test';
 import {HomePage} from '../pages/home'
 
-test('test', async ({ page }) => {
+
+test.beforeEach(async ({ page }) => {
     const Home = new HomePage(page);
     await Home.visitSite();
+  });
+
+test('validationTest', async ({ page }) => {
+    const Home = new HomePage(page);    
     await Home.assertContactFormInfo();     
     await Home.testContactFormValidation();
 });
